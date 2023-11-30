9303e4f63d655a913f8fe162ec3b1e69d10e8fc6
dsardelic <25157560+dsardelic@users.noreply.github.com>
2023-11-30 14:03:20 +0100

    Require zero user configuration prior to starting the app

--- Tagger/urls.py
+++ Tagger/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("pathtagger/", include("pathtagger.urls")),
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(pattern_name="pathtagger:homepage")),
 ]
